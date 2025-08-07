3fb39197c5ca567d413c1b46a970552f6a36b231
joaco545 <joacoplac@gmail.com>
2025-08-07 18:06:22 -0300

    Update MateriaRepository.java

--- abmalumnos/src/main/java/com/abmalumnos/repository/MateriaRepository.java
+++ abmalumnos/src/main/java/com/abmalumnos/repository/MateriaRepository.java
@@ -2,6 +2,8 @@ package com.abmalumnos.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.abmalumnos.Materia;
+
 public interface MateriaRepository extends CrudRepository<Materia, Integer> {
 
 }
