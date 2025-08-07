92531c5cd4d7b0ee4397e2cfedc2617d3fb2bb2c
joaco545 <joacoplac@gmail.com>
2025-08-06 22:32:25 -0300

    Creadas clases que representan la estructura de datos de informacion secundaria de la BD

---
+++ abmalumnos/src/main/java/com/abmalumnos/Carrera.java
@@ -0,0 +1,44 @@
+package com.abmalumnos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+/*
+ * Segun lo especificado por la documentacion, esta clase no
+ * contiene ningun tipo de comportamiento asociado al front,
+ * por lo cual esta no tiene un controlador/API asociada
+ * 
+ * En terminos de que decirle a la profe: Nosotros nos
+ * encargamos del sistema de altas, bajas, y modificaciones
+ * a cuentas de alumnos. Mientras que usamos estas bases de
+ * datos, estas estan por fuera del limite de nuestro sistema
+ * y se asume que hay otro sistema que manipula y mantiene al
+ * dia estas bases de datos
+ */
+
+@Entity
+public class Carrera {
+    @Id
+    private int codigoCarrera;
+    private String nombreCarrera;
+
+    /*
+     * La clase tiene que ser inmutable, los datos se obtienen
+     * de la DB, cuando se llama al constructor de la clase, y
+     * luego todo tiene que ser operaciones read-only (getters)
+     */
+
+    public Carrera(){
+        // TODO: Conectar esto a la base de datos, o pasarle las cosas por parametro
+        this.codigoCarrera = 101010;
+        this.nombreCarrera = "Lorem Ipsum Dorime Ameno Patricio Estrella";
+    }
+
+    //#region Getters
+
+    public int getCodigoCarrera() { return codigoCarrera; }
+
+    public String getNombreCarrera() { return nombreCarrera; }
+
+    //#endregion
+}
---
+++ abmalumnos/src/main/java/com/abmalumnos/Materias.java
@@ -0,0 +1,43 @@
+package com.abmalumnos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+/*
+ * Segun lo especificado por la documentacion, esta clase no
+ * contiene ningun tipo de comportamiento asociado al front,
+ * por lo cual esta no tiene un controlador/API asociada
+ * 
+ * En terminos de que decirle a la profe: Nosotros nos
+ * encargamos del sistema de altas, bajas, y modificaciones
+ * a cuentas de alumnos. Mientras que usamos estas bases de
+ * datos, estas estan por fuera del limite de nuestro sistema
+ * y se asume que hay otro sistema que manipula y mantiene al
+ * dia estas otras bases de datos
+ */
+
+@Entity
+public class Materias {
+    @Id
+    private int codigoMateria;
+    private String nombreMateria;
+
+    /*
+     * La clase tiene que ser inmutable, los datos se obtienen
+     * de la DB, cuando se llama al constructor de la clase, y
+     * luego todo tiene que ser operaciones read-only (getters)
+     */
+
+     public Materias(){
+        // TODO: Conectar esto a la base de datos, o pasarle las cosas por parametro
+        this.codigoMateria = 010101;
+        this.nombreMateria = "Lorem Ipsum Quis custodiet ipsos custodes";
+     }
+
+     //#region Getters
+
+     public int getCodigoMateria() { return codigoMateria; }
+
+     public String getNombreMateria() { return nombreMateria; }
+     //#endregion
+}
