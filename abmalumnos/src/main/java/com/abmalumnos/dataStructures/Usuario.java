cad8e19bf5e1dc6d1d3b67431670ba4311defda4
joaco545 <joacoplac@gmail.com>
2025-08-10 20:21:58 -0300

    Creada la estructura de datos para manejar usuario y contraseña

---
+++ abmalumnos/src/main/java/com/abmalumnos/dataStructures/Usuario.java
@@ -0,0 +1,53 @@
+package com.abmalumnos.dataStructures;
+
+import jakarta.persistence.Entity;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Usuario {
+    @Id
+    private Integer legajo;
+
+    @OneToOne
+    @JoinColumn(name = "legajo")
+    private Alumno alumno;
+
+    private byte[] password;
+
+
+    //#region setters
+
+    public void setLegajo(Integer legajo) { this.legajo = legajo; }
+
+    public void setAlumno(Alumno alumno) { this.alumno = alumno; }
+
+    public void setPassword(String password) {
+        this.password = hashString(password);
+    }
+
+    //#endregion
+
+
+    public boolean tryPassword(String password) {
+        return this.password.equals(hashString(password));
+    }
+
+
+    private byte[] hashString(String toHash) {
+        byte[] ret = null;
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            ret = md.digest(toHash.getBytes());
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return ret;
+    }
+}
