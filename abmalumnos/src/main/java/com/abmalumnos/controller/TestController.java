643d9a028da96fe2fd71257c72603c2f0d957658
joaco545 <joacoplac@gmail.com>
2025-08-13 16:27:24 -0300

    Creados endpoints que devuelven estructuras vacias para ver como son los JSON

---
+++ abmalumnos/src/main/java/com/abmalumnos/controller/TestController.java
@@ -0,0 +1,38 @@
+package com.abmalumnos.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.abmalumnos.dataStructures.*;
+import com.abmalumnos.wrappers.*;
+
+@RestController
+@RequestMapping("/api/test")
+@CrossOrigin(origins = "*")
+public class TestController {
+
+    //#region Estructuras
+
+    @GetMapping("/alumno")
+    public @ResponseBody Alumno getAlumnoStructure() { return new Alumno(); }
+
+    @GetMapping("/carrera")
+    public @ResponseBody Carrera getCarreraStructure() { return new Carrera(); }
+
+    @GetMapping("/materia")
+    public @ResponseBody Materia getMateriaStructure() { return new Materia(); }
+
+    @GetMapping("/usuario")
+    public @ResponseBody Usuario getUsuarioStructure() { return new Usuario(); }
+
+    //#endregion
+
+    //#region Wrappers
+
+    @GetMapping("/logindata")
+    public @ResponseBody LoginDataWrapper getLoginDataWrapperStructure() { return new LoginDataWrapper(); }
+
+    @GetMapping("/passchange")
+    public @ResponseBody PassChangeWrapper getPassChangeWrapperStructure() { return new PassChangeWrapper(); }
+
+    //#endregion
+}
