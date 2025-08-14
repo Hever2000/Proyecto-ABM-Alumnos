b3d1063ad6877341ac37d076baa22aa6c2a2d4cb
joaco545 <joacoplac@gmail.com>
2025-08-14 20:11:22 -0300

    El pass change wrapper test ahora devuelve la estructura completa (con un login data vacio)

--- abmalumnos/src/main/java/com/abmalumnos/wrappers/PassChangeWrapper.java
+++ abmalumnos/src/main/java/com/abmalumnos/wrappers/PassChangeWrapper.java
@@ -5,7 +5,7 @@ public class PassChangeWrapper {
     private String nuevaContra;
 
     public LoginDataWrapper getLoginData() { return loginData; }
-    public void setUsuario(LoginDataWrapper loginData) { this.loginData = loginData; }
+    public void setLoginData(LoginDataWrapper loginData) { this.loginData = loginData; }
 
     public String getNuevaContra() { return nuevaContra; }
     public void setNuevaContra(String nuevaContra) { this.nuevaContra = nuevaContra; }
